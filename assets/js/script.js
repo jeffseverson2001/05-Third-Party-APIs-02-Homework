5e2c06661233d04eee4f63a5954229dcf96006e1
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-12 21:06:33 -0500

    Update Wednesday One

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//  Declair elements
+let currentDate = document.getElementById('currentDay');
+
+
+//  Get current date and time
+
+var now = DateTime.now();
+
+console.log(now);
+
+answerButton.innerHTML = now;
+document.body.appendChild(currentDate);
+
+
+
+//  Start hours from 9am to 5pm
+
+//  Store Event in Local Storage
+
+//  Get event from local storage for hour line
+
+//  Build line for each hour with event and save button
