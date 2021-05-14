b5b6ce341945bedcada1b429c549e671b045d4e9
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-13 21:34:46 -0500

    End Of Class Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,33 @@
 let currentDate = document.getElementById('currentDay');
 
 
+let DateTime = luxon.DateTime;
+
+const dt = DateTime.fromISO(new Date().toISOString());
+console.log(dt.toFormat('yyyy/MM/dd'));
+const dt2 = DateTime.fromJSDate(new Date());
+console.log(dt2.toFormat('yyyy/MM/dd'));
+
+
+
 //  Get current date and time
 
-let now = DateTime.now();
+let DateTime = luxon.DateTime;
+let jDate = DateTime;
+
+
+//let now = "This is now";
+//DateTime.now();
 //let dt = DateTime.local(2017, 5, 15, 8, 30);
 
-console.log(now);
+//console.log(now);
+
+//let jDate = toLocaleString(DateTime.now());
+//let jDate = DateTime.local();
+//let jDate = "J Date";
 
 //jDate.innerHTML = "This is the current Date";
-currentDate.append("jDate");
+//currentDate.append(jDate);
 
 
 //answerButton.innerHTML = now;
